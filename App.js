be7704ab401990b99a6e18a7d5864d029e6d7ea9
Asish-T-Babu <asishtbabu@gmail.com>
2024-01-12 07:33:38 +0530

    first commit

--- App.js
+++ App.js
@@ -1,77 +1,23 @@
-// const heading = React.createElement('h1', {id: "heading"}, "Hello world from react!")
-// const root = ReactDOM.createRoot(document.getElementById('root'))
-// console.log(root,"root")
-// root.render(heading)
-
-// the above line is for creating an html tag with it's value in the index.html file
-////////////////////////////////////////////////////////////////////////////////////////////////
-// now we need to create tree like structure (below is an example) in the index.html file 
-
-{/* <div id='parent'>
-    <div id='child'>
-        <h1>I am an h1 tag!</h1>
-    </div>
-</div>
-
-ReactElement -> is an object ->becomes html when browser understand
-*/}
-// const heading = React.createElement('h1',{},"I am an h1 tag!")
-// const parent = React.createElement(
-//     'div',
-//     {id: 'parent'},
-//     React.createElement('div', {id: 'child'}, heading)
-// )
-// const root = ReactDOM.createRoot(document.getElementById('root'))
-// root.render(parent)
-////////////////////////////////////////////////////////////////////////////////////////////////////
-// right now i need to create sibilings
-
-{/* <div id='parent'>
-    <div id='child'>
-        <h1>I am an h1 tag!</h1>
-        <h2>I am an h2 tag!</h2>
-    </div>
-</div>
-
-ReactElement -> is an object ->becomes html when browser understand
-*/}
-
-// const heading1 = React.createElement('h1',{},"I am an h1 tag!")
-// const heading2 = React.createElement('h2',{},"I am an h2 tag!")
-// const parent = React.createElement(
-//     'div',
-//     {id: 'parent'},
-//     React.createElement('div', {id: 'child'}, [heading1, heading2])
-// )
-// const root = ReactDOM.createRoot(document.getElementById('root'))
-// root.render(parent)
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// if i need to create more complex structure
-{/* <div id='parent'>
-    <div id='child1'>
-        <h1>I am an h1 tag!</h1>
-        <h2>I am an h2 tag!</h2>
-    </div>
-    <div id='child2'>
-        <h1>I am an h1 tag!</h1>
-        <h2>I am an h2 tag!</h2>
-    </div>
-</div>*/}
-
 import React from "react"
 import ReactDOM from "react-dom"
-const heading1 = React.createElement('h1',{},"I am an h1 tag!")
-const heading2 = React.createElement('h2',{},"I am an h2 tag!")
-const parent = React.createElement(
-    'div',
-    {id: 'parent'},
-    [React.createElement('div', {id: 'child1'}, [heading1, heading2]),React.createElement('div', {id: 'child2'}, [heading1, heading2])]
-)
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(parent)
 
-//jsx
-// we can make the above code more eaiser with jsx
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// const heading = React.createElement('h1',{},"I am an h1 tag!")
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(heading)
+
+// who can i create ReactElement without createElement function
+
+const heading = <h1> React using JSX </h1>
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(heading)
+
+// some developers might thing we are writing html in JavaScript- the answer is no the above line is JSX not HTML
+
+// The JSX is a HTML like or XML like syntax
+
+// how the above code is running?
+// the above code is transplied into ReactElement(JS object)
+// the end of the day it is createElement in react = It is the same thing as the above commend line of codes
+
+
+// The Babel is converting JSX code into JS
